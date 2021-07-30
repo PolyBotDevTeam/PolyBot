3094ab403b7b5b78f76f521f30855756b7e18255
PolyBot Dev Team <polybotdevteam@gmail.com>
2021-07-30 05:29:16 +0000

    Add FFAGame class

---
+++ polybot_database/_ffa_game.py
@@ -0,0 +1,73 @@
+import utils as _utils
+
+
+class FFAGame:
+
+    def __init__(self, *, game_id, execute_query):
+        self._execute = execute_query
+        self._game_id = game_id
+        self._errors = _FFAGameErrors()
+        # TODO: Probably should add _ensure_exists
+
+    @protected
+    def errors(self):
+        return self._errors
+
+    def exists(self):
+        raise NotImplementedError
+
+    def add_player(self, player_id):
+        if self.has_member(player_id):
+            raise self.errors.AlreadyMemberError('this player has already joined the game')
+        self._execute(
+            'INSERT ffa_memberships(member_id, game_id) VALUES (%s, %s);',
+            [player_id, game_id]
+        )
+
+    def remove_player(self, player_id):
+        if not self.has_member(player_id):
+            raise self.errors.NotMemberError('this player is not in the game')
+        self._execute(
+            'DELETE FROM ffa_memberships WHERE member_id = %s AND game_id = %s;',
+            [player_id, game_id]
+        )
+
+    def has_member(self, player_id):
+        [[result]] = self._execute(
+            'SELECT EXISTS'
+            '(SELECT * FROM ffa_memberships WHERE member_id = %s AND game_id = %s);'
+            [player_id, game_id]
+        )
+        return result
+
+    # Probably should not use "id" in the method name
+    # because it sounds more like implementation detail.
+    # It's easier to think about abstract members rather than their ids.
+    def get_members(self):
+        response = self._execute(
+            'SELECT member_id FROM ffa_memberships WHERE game_id = %s;',
+            game_id
+        )
+        [members_ids] = _utils.safe_zip(*response)
+        return members_ids
+
+    def start(self, game_name):
+        raise NotImplementedError
+
+    def finish(self, winner_id):
+        raise NotImplementedError
+
+    def delete(self):
+        raise NotImplementedError
+
+
+class _FFAGameErrors:
+
+    def __init__(self):
+        pass
+
+    class AlreadyMemberError(ValueError):
+        pass
+
+    class NotAMemberError(ValueError):
+        pass
--- polybot_database/_ffa_games.py
+++ polybot_database/_ffa_games.py
@@ -1,6 +1,10 @@
+import polybot_database._ffa_game as _ffa_game_module
+
+
+# TODO: Rename this class
 class FFAGames:
 
-    def __init__(self, execute_query):
+    def __init__(self, *, execute_query):
         self._execute = execute_query
         self._errors = _FFAGamesErrors()
 
@@ -8,66 +12,19 @@ class FFAGames:
     def errors(self):
         return self._errors
 
+    def get_game_by_id(self, game_id):
+        game = _ffa_game_module.FFAGame(game_id=game_id, execute_query=self._execute)
+        if not game.exists():
+            raise self.errors.GameNotFoundError('the game with this id does not exist')
+        return game
+
     def create_game(self, owner_id, description):
         [[game_id]] = self._execute(
             'INSERT ffa_games(owner_id, description) VALUES (%s, %s);\n'
             'SELECT LAST_INSERT_ID();'
             [owner_id, description]
         )
-        return game_id
-
-    def delete_game(self, game_id):
-        raise NotImplementedError
-
-    def does_game_exist(self, game_id):
-        raise NotImplementedError
-
-    def ensure_game_exists(self, game_id):
-        if not self.does_game_exist(game_id):
-            raise self.errors.GameNotFoundError('the game with this id does not exist')
-
-    def add_player_to_game(self, player_id, game_id):
-        if self.is_member(player_id, game_id):
-            raise self.errors.AlreadyMemberError('this player has already joined the game')
-        self._execute(
-            'INSERT ffa_memberships(member_id, game_id) VALUES (%s, %s);',
-            [player_id, game_id]
-        )
-
-    def remove_player_from_game(self, player_id, game_id):
-        if not self.is_member(player_id, game_id):
-            raise self.errors.NotMemberError('this player is not in the game')
-        self._execute(
-            'DELETE FROM ffa_memberships WHERE member_id = %s AND game_id = %s;',
-            [player_id, game_id]
-        )
-
-    def is_member(self, player_id, game_id):
-        self.ensure_game_exists(game_id)
-        [[result]] = self._execute(
-            'SELECT EXISTS'
-            '(SELECT * FROM ffa_memberships WHERE member_id = %s AND game_id = %s);'
-            [player_id, game_id]
-        )
-        return result
-
-    # Probably should not use "id" in the method name
-    # because it sounds more like implementation detail.
-    # It's easier to think about abstract members rather than their ids.
-    def get_game_members(self, game_id):
-        self.ensure_game_exists(game_id)
-        response = self._execute(
-            'SELECT member_id FROM ffa_memberships WHERE game_id = %s;',
-            game_id
-        )
-        [members_ids] = utils.safe_zip(*response)
-        return members_ids
-
-    def start_game(self, game_id, game_name):
-        raise NotImplementedError
-
-    def finish_game(self, game_id, winner):
-        raise NotImplementedError
+        return self.get_game_by_id(game_id)
 
     def get_games_of_player(self, player_id):
         raise NotImplementedError
@@ -79,10 +36,4 @@ class _FFAGamesErrors:
         pass
 
     class GameNotFoundError(ValueError):
-        pass
-
-    class AlreadyMemberError(ValueError):
-        pass
-
-    class NotAMemberError(ValueError):
         pass
