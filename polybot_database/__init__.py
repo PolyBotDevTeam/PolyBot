90d47e181299e1dc559982ee6d33343a32280e34
PolyBot Dev Team <polybotdevteam@gmail.com>
2021-08-01 05:40:52 +0000

    Now PolyBotDatabase creates a new connection for each query\n\nThis commit fixes several issues, mentioned in the comments for PolyBotDatabase._execute_query

--- polybot_database/__init__.py
+++ polybot_database/__init__.py
@@ -12,7 +12,6 @@ class PolyBotDatabase:
     #       instead of create_connection
     def __init__(self, create_connection):
         self._create_connection = create_connection
-        self._connection = self._create_connection()
         self._ffa_games = _ffa_games_module.FFAGames(execute_query=self._execute_query)
 
     @property
@@ -25,9 +24,13 @@ class PolyBotDatabase:
         return self._create_connection
 
     def _execute_query(self, query, args=None):
-        # TODO: probably should also create new connection here
-        #       Reason: https://stackoverflow.com/questions/14827783/auto-increment-and-last-insert-id/14827987#14827987
-        cursor = self._connection.cursor()
+        # The reason why should create a new connection:
+        # it's bad idea to use the same connection for all queries
+        # https://stackoverflow.com/questions/14827783/auto-increment-and-last-insert-id/14827987#14827987
+        # Another reason - it's the simplest way to avoid this:
+        # https://dev.mysql.com/doc/refman/5.7/en/gone-away.html
+        connection = self._create_connection()
+        cursor = connection.cursor()
         cursor.execute(query, args)
         return cursor
 
