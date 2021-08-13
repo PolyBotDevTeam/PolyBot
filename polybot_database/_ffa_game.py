7172abe3e103855ee797d912deb9e4a96a7e24f7
PolyBot Dev Team <polybotdevteam@gmail.com>
2021-08-13 18:20:03 +0000

    Implement start/finish logic for FFAGame

--- polybot_database/_ffa_game.py
+++ polybot_database/_ffa_game.py
@@ -3,6 +3,8 @@ import utils as _utils
 
 class FFAGame:
 
+    _WINNER_ID_FOR_DRAW = 0
+
     def __init__(self, *, game_id: int, execute_query):
         self._execute = execute_query
         self._game_id = game_id  # TODO: Probably should rename to just self._id
@@ -56,14 +58,44 @@ class FFAGame:
         [members_ids] = _utils.safe_zip(*response)
         return members_ids
 
-    def start(self, game_name: str):
-        raise NotImplementedError
-
-    def finish(self, winner_id: int):
-        raise NotImplementedError
-
     def delete(self):
         raise NotImplementedError
+
+    def start(self, game_name: str):
+        if self.is_started():
+            raise self.errors.AlreadyStartedError('the game is already started')
+        self._set_field('game_name', game_name)
+
+    def is_started(self):
+        game_name = self._get_field('game_name')
+        return game_name is not None
+
+    def finish(self, winner_id: int):
+        if not self.is_started():
+            raise self.errors.NotStartedError('unable to finish a game that has not been started')
+        if self.is_finished():
+            raise self.errors.AlreadyFinishedError('the game is already finished')
+        self._set_field('winner_id', winner_id)
+
+    def is_finished(self):
+        winner = self._get_field('winner_id')
+        return winner is not None
+
+    def finish_with_draw(self):
+        return self.finish(winner_id=self._WINNER_ID_FOR_DRAW)
+
+    def _get_field(self, field_name):
+        [[field_value]] = self._execute(
+            'SELECT %s FROM ffa_games WHERE game_id = %s;',
+            [field_name, self._game_id]
+        )
+        return field_value
+
+    def _set_field(self, field_name, new_value):
+        self._execute(
+            'UPDATE ffa_games SET %s = %s WHERE game_id = %s;',
+            [field_name, new_value, self._game_id]
+        )
 
 
 class _FFAGameErrors:
@@ -75,4 +107,13 @@ class _FFAGameErrors:
         pass
 
     class NotAMemberError(ValueError):
+        pass
+
+    class AlreadyStartedError(ValueError):
+        pass
+
+    class NotStartedError(ValueError):
+        pass
+
+    class AlreadyFinishedError(ValueError):
         pass
