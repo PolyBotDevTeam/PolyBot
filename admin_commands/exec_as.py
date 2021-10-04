ccc430489669e4e8251b66848678bf590b931359
PolyBot Dev Team <polybotdevteam@gmail.com>
2021-10-04 15:16:47 +0000

    Make exec_as command newstyle command

--- admin_commands/exec_as.py
+++ admin_commands/exec_as.py
@@ -3,7 +3,7 @@ import utils
 import vk_utils
 
 
-def exec_as(player_id, command_text):
+def _process_exec_as_command(player_id, command_text, **kwargs):
     command_text = command_text.lstrip()
     try:
         player_link, command_to_exec = utils.split_one(command_text)
@@ -23,11 +23,13 @@ def exec_as(player_id, command_text):
         return ['Команда с таким именем не найдена.']
     c = user_commands.get_command(command_name)
 
-    return c.process(target_player_id, command_text)
+    return c(target_player_id, command_text, **kwargs)
 
 
-exec_as_command = command_system.AdminCommand()
-
-exec_as_command.keys = ['exec_as', 'as']
-exec_as_command.description = ' упоминание_игрока команда - Выполнить команду от лица указанного игрока.'
-exec_as_command.process = exec_as
+exec_as_command = command_system.Command(
+    process=_process_exec_as_command,
+    keys=['exec_as', 'as'],
+    description='Выполнить команду от лица указанного игрока',
+    signature='упоминание_игрока команда',
+    allow_users=False
+)
