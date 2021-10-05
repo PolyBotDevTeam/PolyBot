274a5d9ca4e9f15be442cd97b6a7041234b4a62b
PolyBot Dev Team <polybotdevteam@gmail.com>
2021-10-05 10:02:35 +0000

    Improve message about game start

--- commands/start_ffa.py
+++ commands/start_ffa.py
@@ -1,9 +1,10 @@
 import command_system
 import polybot_responses as responses
 import utils
+import vk_utils
 
 
-def start_ffa(actor, game_id, game_name, *, database):
+def start_ffa(actor, game_id, game_name, *, database, vk):
     ffa_games = database.ffa_games
     try:
         game = ffa_games.get_game_by_id(game_id)
@@ -24,10 +25,19 @@ def start_ffa(actor, game_id, game_name, *, database):
             error_message = responses.EMPTY_GAME_NAME_ERROR
         return [error_message]
 
-    return [responses.FFA_GAME_STARTED.format(game_id=game.id)]
+    members_mentions = [vk_utils.create_mention(member_id, vk=vk) for member_id in game.members]
+    members_mentions = '\n'.join(f'* {mention}' for mention in members_mentions)
+
+    message_about_start = responses.FFA_GAME_STARTED.format(
+        game_id=game.id,
+        name=game.name,
+        members_mentions=members_mentions
+    )
+
+    return [message_about_start]
 
 
-def _process_start_ffa_command(actor, command_text, *, database, **kwargs):
+def _process_start_ffa_command(actor, command_text, *, database, vk, **kwargs):
     if not command_text:
         return [responses.START_FFA_NO_ARGUMENTS_ERROR]
 
@@ -38,7 +48,7 @@ def _process_start_ffa_command(actor, command_text, *, database, **kwargs):
     except ValueError:
         return [responses.INVALID_ID_SYNTAX_ERROR]
 
-    return start_ffa(actor, game_id, game_name, database=database)
+    return start_ffa(actor, game_id, game_name, database=database, vk=vk)
 
 
 start_ffa_command = command_system.Command(
