57951cf19247b13c13d4da0780778929dfd69eec
PolyBot Dev Team <polybotdevteam@gmail.com>
2021-10-11 21:44:32 +0000

    Make "/top" command newstyle command

--- commands/top.py
+++ commands/top.py
@@ -1,6 +1,6 @@
 import command_system
-import message_handler
 import elo as elo_module
+import vk_utils
 
 
 def _is_int(x):
@@ -12,7 +12,43 @@ def _is_int(x):
     return result
 
 
-def top(player_id, command_text):
+def top(count, *, category, cursor, vk):
+    title = f'ТОП-{count}'
+
+    elo_module.recalculate(cur=cursor)
+
+    if count == 0:
+        ids = []
+        elos_str = []
+    elif category in ('', 'сумма', 'sum'):
+        cursor.execute('SELECT player_id, host_elo, elo FROM players WHERE host_elo + elo > 2000 ORDER BY (host_elo + elo) DESC;')
+        rows = cursor.fetchmany(min(count, cursor.rowcount))
+        ids, *elos_rows = zip(*rows)
+        elos_str = [str(host_elo) + ' / ' + str(away_elo) for host_elo, away_elo in zip(*elos_rows)]
+    else:
+        if category in ('хост', 'host', 'первый', 'first'):
+            title += ' (хост)'
+            cursor.execute('SELECT player_id, host_elo FROM players WHERE host_elo > 1050 ORDER BY host_elo DESC;')
+        elif category in ('второй', 'away', 'second'):
+            title += ' (второй)'
+            cursor.execute('SELECT player_id, elo FROM players WHERE elo > 950 ORDER BY elo DESC;')
+        else:
+            message = 'Неправильный формат ввода. Попробуйте просто /топ.'
+            return [message]
+        rows = cursor.fetchmany(min(count, cursor.rowcount))
+        ids, elos = zip(*rows)
+        elos_str = [str(elo) for elo in elos]
+
+    message = f'{title}:\n\n'
+
+    usernames = vk_utils.fetch_usernames(ids, vk=vk)
+    for place, (username, elo_str) in enumerate(zip(usernames, elos_str), 1):
+        message += f'{place}. {username}: {elo_str} ЭЛО\n'
+
+    return [message]
+
+
+def _process_top_command(player_id, command_text, *, cursor, vk, **kwargs):
     args = command_text.split()
 
     count = None
@@ -29,46 +65,17 @@ def top(player_id, command_text):
     if count < 0:
         count = 0
 
-    title = f'ТОП-{count}'
+    [category] = args if args else ['sum']
 
-    connection = message_handler.create_connection()
-    with connection:
-        cur = connection.cursor()
-        elo_module.recalculate(cur=cur)
+    result = top(count, category=category, cursor=cursor, vk=vk)
 
-        if count == 0:
-            ids = []
-            elos_str = []
-        elif not args:
-            cur.execute('SELECT player_id, host_elo, elo FROM players WHERE host_elo + elo > 2000 ORDER BY (host_elo + elo) DESC;')
-            rows = cur.fetchmany(min(count, cur.rowcount))
-            ids, *elos_rows = zip(*rows)
-            elos_str = [str(host_elo) + ' / ' + str(away_elo) for host_elo, away_elo in zip(*elos_rows)]
-        else:
-            if args[0] in ('хост', 'host', 'первый', 'first'):
-                title += ' (хост)'
-                cur.execute('SELECT player_id, host_elo FROM players WHERE host_elo > 1050 ORDER BY host_elo DESC;')
-            elif args[0] in ('второй', 'away', 'second'):
-                title += ' (второй)'
-                cur.execute('SELECT player_id, elo FROM players WHERE elo > 950 ORDER BY elo DESC;')
-            else:
-                message = 'Неправильный формат ввода. Попробуйте просто /топ.'
-                return [message]
-            rows = cur.fetchmany(min(count, cur.rowcount))
-            ids, elos = zip(*rows)
-            elos_str = [str(elo) for elo in elos]
-
-    message = f'{title}:\n\n'
-
-    usernames = message_handler.fetch_usernames(ids)
-    for place, (username, elo_str) in enumerate(zip(usernames, elos_str), 1):
-        message += f'{place}. {username}: {elo_str} ЭЛО\n'
-
-    return [message]
+    return result
 
 
-top_command = command_system.UserCommand()
-
-top_command.keys = ['топ', 'top']
-top_command.description = ' (_ / хост / второй) - Игроки с наивысшим ЭЛО-рейтингом.'
-top_command.process = top
+top_command = command_system.Command(
+    process=_process_top_command,
+    keys=['топ', 'top'],
+    description='Игроки с наивысшим ЭЛО-рейтингом',
+    signature='(_ / хост / второй)',
+    allow_users=True
+)
