d4592479df33a19bfcc5e18845079951376c074f
PolyBot Dev Team <polybotdevteam@gmail.com>
2021-10-23 23:38:24 +0000

    Allow reply with the "/nickname" command to get the reply recipient nickname

--- commands/nickname.py
+++ commands/nickname.py
@@ -3,10 +3,8 @@ import db_utils
 import message_handler
 
 
-def show_nickname(actor_id, command_text, *, cursor, **kwargs):
-    if not command_text:
-        target_player_id = actor_id
-    else:
+def show_nickname(actor_id, command_text, *, cursor, actor_message, **kwargs):
+    if command_text:
         try:
             target_player_id = message_handler.try_to_identify_id(command_text, cursor)
         except ValueError:
@@ -15,7 +13,13 @@ def show_nickname(actor_id, command_text, *, cursor, **kwargs):
             # TODO: Can find at least 3 such messages in this project.
             #       Probably should move it out.
             return ['Не удалось обнаружить пользователя по введённым данным.']
-    
+
+    elif actor_message is not None and 'reply_message' in actor_message.keys():
+        target_player_id = actor_message['reply_message']['from_id']
+
+    else:
+        target_player_id = actor_id
+
     cursor.execute('SELECT nickname FROM players WHERE player_id = %s;', target_player_id)
     [[nickname]] = cursor
     return [nickname]
@@ -28,4 +32,3 @@ nickname_command = command_system.Command(
     signature='игрок',
     allow_users=True
 )
-
