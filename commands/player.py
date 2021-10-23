93ed931cdd3060d41d74cc17d7f491687c157912
PolyBot Dev Team <polybotdevteam@gmail.com>
2021-10-23 23:11:29 +0000

    Refactor inner names of the "/player" command

--- commands/player.py
+++ commands/player.py
@@ -6,16 +6,11 @@ import db_utils
 
 
 def _process_player_command(actor_id, command_text, *, cursor, **kwargs):
-    player_called_id = actor_id
-    del actor_id
-
-    pointer = command_text if command_text else None
+    player_to_describe_info = command_text if command_text else None
     del command_text
 
-    cur = cursor
-
     try:
-        player_to_desc_id = message_handler.try_to_identify_id(pointer, cur) if pointer is not None else player_called_id
+        player_to_describe_id = message_handler.try_to_identify_id(player_to_describe_info, cursor) if player_to_describe_info is not None else actor_id
     except vk_utils.InvalidMentionError:
         message = 'Некорректная ссылка. Нажмите @ или *, чтобы выбрать среди участников беседы.'
         return [message]
@@ -23,21 +18,21 @@ def _process_player_command(actor_id, command_text, *, cursor, **kwargs):
         message = 'Не удалось обнаружить пользователя по введённым данным.'
         return [message]
 
-    if not db_utils.exists(cur, 'players', 'player_id = %s', player_to_desc_id):
-        if pointer is None:
+    if not db_utils.exists(cursor, 'players', 'player_id = %s', player_to_describe_id):
+        if player_to_describe_info is None:
             message = 'Вы ещё не зарегистрированы в системе.'
         else:
             message = 'Этот пользователь ещё не зарегистрирован в системе.'
         return [message]
 
-    elo.recalculate(cur=cur)
-    message = _compose_description(player_to_desc_id, cur)
+    elo.recalculate(cur=cursor)
+    message = _compose_description(player_to_describe_id, cursor)
     return [message]
 
 
-def _compose_description(player_id, cur):
-    cur.execute('SELECT player_id, role, host_elo, elo, nickname, joining_time FROM players WHERE player_id = %s', player_id)
-    _player_id, role, elo_host, elo_away, nickname, joining_time = cur.fetchone()
+def _compose_description(player_id, cursor):
+    cursor.execute('SELECT player_id, role, host_elo, elo, nickname, joining_time FROM players WHERE player_id = %s', player_id)
+    _player_id, role, elo_host, elo_away, nickname, joining_time = cursor.fetchone()
     username = message_handler.username(player_id)
     role = role if role is not None else 'Игрок'
     desc = f'{username} - {role}\nЭЛО: {elo_host} : {elo_away}\nНикнейм: {nickname}\nДата регистрации: {joining_time}'
