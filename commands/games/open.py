e43ed4df604b60a553e021d932c8d3aae3b07ec9
PolyBot Dev Team <polybotdevteam@gmail.com>
2021-11-14 04:25:17 +0000

    Make the "/open" command newstyle command

--- commands/games/open.py
+++ commands/games/open.py
@@ -3,7 +3,7 @@ import message_handler
 import vk_utils
 
 
-def open(player_id, command_text):
+def _process_open_command(actor_id, command_text, **kwargs):
     description = command_text
     if not description:
         message = 'Необходимо ввести описание игры. Это может быть имя игрока с которым вы хотите сыграть, минимальный/максимальный рейтинг для вступления, какие-либо дополнительные правила на ваше усмотрение. По умолчанию игра создаётся на карте размера Normal в режиме Might.'
@@ -13,13 +13,13 @@ def open(player_id, command_text):
     with connection:
         cur = connection.cursor()
 
-        cur.execute('SELECT player_id FROM players WHERE player_id = %s', (player_id,))
+        cur.execute('SELECT player_id FROM players WHERE player_id = %s', actor_id)
         fetch = cur.fetchone()
         if not fetch:
             message = 'Вы не зарегистрированы в системе. Воспользуйтесь командой /гайд, чтобы узнать о работе бота.'
             return [message]
 
-        cur.execute('SELECT game_id FROM games WHERE host_id = %s AND type = \'r\'', (player_id,))
+        cur.execute('SELECT game_id FROM games WHERE host_id = %s AND type = \'r\'', actor_id)
         fetch = cur.fetchone()
         if fetch:
             ready = fetch[0]
@@ -27,13 +27,13 @@ def open(player_id, command_text):
                 message = f'Нельзя открыть игру. Игра {ready} ждёт своего начала. Создайте игру в Политопии и используйте команду /начать ID_игры название_игры, чтобы получить возможность открывать новые игры.'
                 return [message]
 
-        cur.execute('SELECT COUNT(1) FROM games WHERE host_id = %s AND type = \'o\'', (player_id,))
+        cur.execute('SELECT COUNT(1) FROM games WHERE host_id = %s AND type = \'o\'', actor_id)
         opened = cur.fetchone()[0]
         if opened > 2:
             message = 'Вы уже открыли 3 игры. Подождите их заполнения, перед тем как открывать новые.'
             return [message]
 
-        cur.execute('INSERT games(type, host_id, description, time_updated) VALUES (\'o\', %s, %s, NOW());', (player_id, description))
+        cur.execute('INSERT games(type, host_id, description, time_updated) VALUES (\'o\', %s, %s, NOW());', (actor_id, description))
         cur.execute('SELECT MAX(game_id) FROM games')
         new_id = cur.fetchone()[0]
 
@@ -42,8 +42,10 @@ def open(player_id, command_text):
     return [message]
 
 
-open_command = command_system.UserCommand()
-
-open_command.keys = ['открыть', 'создать', 'open']
-open_command.description = ' описание - Создать игру с вами в роли хоста. Другие игроки смогут вступить в неё.'
-open_command.process = open
+open_command = command_system.Command(
+    process=_process_open_command,
+    keys=['открыть', 'создать', 'open'],
+    description='Создать игру с вами в роли хоста. Другие игроки смогут вступить в неё',
+    signature='описание',
+    allow_users=True
+)
