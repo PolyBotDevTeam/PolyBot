c1a92d36223f043968f8438b8cc89ba29bf2cafd
PolyBot Dev Team <polybotdevteam@gmail.com>
2022-03-24 14:10:15 +0000

    Improve "/hey" command keys

--- commands/hey.py
+++ commands/hey.py
@@ -7,7 +7,7 @@ def _process_hey_command(actor_id, command_text, **kwargs):
 
 hey_command = command_system.Command(
     process=_process_hey_command,
-    keys=['эй', 'ало', 'алло', 'алё', 'hey'],
+    keys=['ау', 'эй', 'ало', 'алло', 'алё', 'тест', 'hey', 'test'],
     description='Поторопить бота',
     signature='',
     allow_users=True
