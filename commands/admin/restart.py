aabbe9488967552e3da798805008abec2d71c8e5
PolyBot Dev Team <polybotdevteam@gmail.com>
2022-05-06 13:47:30 +0000

    Make the "!restart" command newstyle command

--- commands/admin/restart.py
+++ commands/admin/restart.py
@@ -2,13 +2,15 @@ import command_system
 import sys
 
 
-def restart(player_id, command_text):
+def _process_restart_command(player_id, command_text, **kwargs):
     # TODO: yield RestartAction()
     sys.exit()
 
 
-restart_command = command_system.AdminCommand()
-
-restart_command.keys = ['restart']
-restart_command.description = ' - Остановить процесс с полиботом, чтобы он запустился заново.'
-restart_command.process = restart
+restart_command = command_system.Command(
+    process=_process_restart_command,
+    keys=['restart'],
+    description='Перезапустить бота',
+    signature='',
+    allow_users=False
+)
