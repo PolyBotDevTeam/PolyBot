8fe26ae54591d650a2029e43b947b8227d3c52b0
PolyBot Dev Team <polybotdevteam@gmail.com>
2022-12-06 06:07:21 +0000

    Clear db_utils.py from garbage

--- db_utils.py
+++ db_utils.py
@@ -9,30 +9,7 @@ def exists(cursor, table_name, condition, args=None):
     return cursor.fetchone() is not None
 
 
-# TODO: deprecated
+# TODO: Remove (deprecated)
 def select(cursor, query, args=None):
     cursor.execute(query, args)
     return tuple( cursor.fetchall() )
-
-
-# TODO: replace deprecated select with this
-def new_select(cursor, params, table_name, condition=None, args=None):
-    condition_part = f' WHERE {condition}' if condition is not None else ''
-    query = f'SELECT {params} FROM {table_name}{condition_part};'
-    cursor.execute(query, args)
-    return cursor.fetchall()
-del new_select
-
-
-"""
-def exists(cursor, table_name, condition, args=None):
-    response = new_select(cursor, '*', table_name, condition, args)
-    return len(response) > 0
-
-
-class PyMySQLWrapper:
-
-    def execute(self, query, args):
-        pass
-"""
-
